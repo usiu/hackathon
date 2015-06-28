d32cd1b47d6857ee32aa362c496db81a97da1a36
Paul Bombo <paul.bombo@gmail.com>
2015-06-28 07:46:27 +0300

    Added methods to auth.js

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -1,78 +1,85 @@
 import request from "superagent";
 import $ from "jquery";
 import API_CONFIG from "./../../api-config";
-window.$ = $;
 
 export default {
 
     login: function(identifier, password, remember, callback) {
-
-        request
-            .post(API_CONFIG.baseUrl + '/local-login')
-            .set('Content-Type', 'application/json')
-            .withCredentials()
-            .send({
-                email: identifier,
-                password: password,
-                remember: remember
-            })
-            .end(function(err, res) {
-                if (err || res.statusType > 2) {
-                    callback(false, res);
-                } else {
-                    return callback(true, res);
-                }
-            });
-
+      request
+        .post(API_CONFIG.baseUrl + '/local-login')
+        .set('Content-Type', 'application/json')
+        .withCredentials()
+        .send({
+          email: identifier,
+          password: password,
+          remember: remember
+        })
+        .end(function(err, res) {
+        if (err || res.statusType > 2) {
+          callback(false, res);
+        } else {
+          return callback(true, res);
+        }
+        });
     },
 
     logout: function(callback) {
-        request
-            .get(API_CONFIG.baseUrl + '/logout')
-            .set('Content-Type', 'application/json')
-            .withCredentials()
-            .end(function(err, res) {
-                if (err || res.statusType > 2) {
-                    callback(false, res);
-                } else {
-                    callback(true, res);
-                }
-            });
+      request
+        .get(API_CONFIG.baseUrl + '/logout')
+        .set('Content-Type', 'application/json')
+        .withCredentials()
+        .end(function(err, res) {
+          if (err || res.statusType > 2) {
+              callback(false, res);
+          } else {
+              callback(true, res);
+          }
+        });
     },
 
     isLoggedIn: function(callback) {
-        var loggedIn;
-        $.ajax({
-            url: API_CONFIG.baseUrl + '/auth-status',
-            crossDomain: true,
-            xhrFields: { withCredentials: true },
-			cache: false,
-            async: false,
-		})
-            .success(function(response) {
-                if (response.message === 'Authorized') {
-                    loggedIn = true;
-                } else {
-                    loggedIn = false;
-                }
-            })
-        return loggedIn;
+      var loggedIn;
+      $.ajax({
+        url: API_CONFIG.baseUrl + '/auth-status',
+        crossDomain: true,
+        xhrFields: { withCredentials: true },
+        cache: false,
+        async: false,
+      })
+      .success(function(response) {
+        if (response.message === 'Authorized') {
+          loggedIn = true;
+        } else {
+          loggedIn = false;
+        }
+      });
+      return loggedIn;
     },
 
-    getToken: function() {
-
+    getProfile: function() {
+      request
+        .get(API_CONFIG.baseUrl + '/api/v1/users/me')
+        .set('Content-Type', 'application/json')
+        .withCredentials()
+        .end(function(err, res) {
+            if (err || res.statusType > 2) {
+              return false;
+            } else {
+              return res;
+            }
+        });
     },
 
     signup: function(data, context, callback) {
-        request
-            .post(API_CONFIG.baseUrl + '/local-signup')
-            .send(data)
-            .end(function(err, res) {
-                if (err || res.statusType > 2) {
-                    callback(false, res);
-                } else {
-                    return callback(true, res);
-                }
-            });
+      request
+        .post(API_CONFIG.baseUrl + '/local-signup')
+        .send(data)
+        .end(function(err, res) {
+            if (err || res.statusType > 2) {
+                callback(false, res);
+            } else {
+                return callback(true, res);
+            }
+        });
     }
 }
