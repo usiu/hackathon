d6e9728430d1360298589a93745f7c78a52f9792
Paul Bombo <paul.bombo@gmail.com>
2015-07-04 19:13:00 +0300

    User Model changes

--- app/models/user.js
+++ app/models/user.js
@@ -2,13 +2,16 @@ import Backbone from "backbone";
 
 export default Backbone.Model.extend({
     defaults: {
-        email: '',
         gender: '',
         verification: '',
         primary_email: '',
         name: '',
         phone_number: '',
         profile_url: '',
-        date_registered: ''
+        date_registered: '',
+        university: '',
+        hacking_what: '',
+        favourite_os: '',
+        age: ''
     }
 });
