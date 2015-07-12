c300b468ba9fb3a78136ea3fb3616c3a48c2c56f
Paul Bombo <paul.bombo@gmail.com>
2015-07-13 00:27:08 +0300

    [Fix] API config issue with gulp production build

--- gulp/tasks/write-api-configs-production.js
+++ gulp/tasks/write-api-configs-production.js
@@ -7,7 +7,9 @@ gulp.task('api-configs-production', function() {
 	var contents = 'var config = \'' + JSON.stringify(config.apiConfigs.production, null, null) + '\'' + '\n' + 'module.exports = JSON.parse(config)';
 	fs.writeFile(path.join(config.sourceDirectory, '/api-config.js'), contents, function (err) {
   		if (err) throw err;
+		gulp.src(config.sourceDirectory + '/api-config.js')
+			.pipe(gulp.dest(config.distDirectory));
 		return gulp.src(config.sourceDirectory + '/api-config.js')
-			.pipe(gulp.dest(config.distDirectory));		
+			.pipe(gulp.dest('.'));
 	});
 });
